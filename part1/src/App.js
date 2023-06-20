a97c21bd71a7732821dea8dc597e28becd795864
Yousef <yousefsameh24@gmail.com>
2023-06-21 00:47:53 +0300

    pass props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,23 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Hamada" age={12+10}/>
+      <Hello name="Sha3bola" age="10"/>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
 
 export default App
