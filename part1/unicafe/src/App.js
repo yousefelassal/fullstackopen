8d19d683a6c877e6867e14c2527297b78b2f193d
Yousef <yousefsameh24@gmail.com>
2023-06-23 15:11:33 +0300

    event handling

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,18 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+import {useState} from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={21} />
-      <Hello name={name} age={age} />
+      <div>{counter}</div>
+
+      <button onClick={() => console.log('clicked')}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
     </div>
   )
 }
