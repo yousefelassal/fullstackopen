dd0ba3addd483238b998903443b04e6fe1730d0a
Yousef <yousefsameh24@gmail.com>
2023-06-25 14:32:59 +0300

    assigning obj directly using the setter

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,21 +5,11 @@ const App = () => {
     left: 0, right: 0
   })
 
-  const handleLeftClick = () => {
-  const newClicks = { 
-    ...clicks, 
-    left: clicks.left + 1 
-  }
-  setClicks(newClicks)
-}
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
 
-const handleRightClick = () => {
-  const newClicks = { 
-    ...clicks, 
-    right: clicks.right + 1 
-  }
-  setClicks(newClicks)
-}
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
