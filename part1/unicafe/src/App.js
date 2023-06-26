ae2b23182ca27f0d1f92b069a09e773601094df7
Yousef <yousefsameh24@gmail.com>
2023-06-26 18:10:11 +0300

    conditional rendering

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,33 +1,51 @@
-import {useState} from 'react'
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
 
-  const [total, setTotal] = useState(0)
-
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(l => l + 1)
-    setTotal(t => t + 1)
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(r => r + 1)
-    setTotal(t => t + 1)
+    setRight(right + 1)
   }
 
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
-      {right}
-      <p>{allClicks.join(' ')}</p>
 
-      <p>total {total}</p>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
