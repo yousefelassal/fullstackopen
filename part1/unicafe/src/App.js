79c226f3e08a7ed47797fce9f81a029a4f29e2cb
Yousef <yousefsameh24@gmail.com>
2023-06-26 17:04:40 +0300

    queueing a series of state updates
    using a function with the event handler

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,19 +3,20 @@ import {useState} from 'react'
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
-
   const [allClicks, setAll] = useState([])
 
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    setLeft(l => l + 1)
+    setTotal(left + 1 +  right) 
   }
-
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    setRight(r => r + 1)
+    setTotal(left + right + 1)
   }
 
   return (
@@ -24,8 +25,9 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-
       <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }
