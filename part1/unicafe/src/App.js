0382eb10cb80476af3fbfae0a3e13216f8e87c82
Yousef <yousefsameh24@gmail.com>
2023-06-26 22:13:27 +0300

    solve 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,51 +1,62 @@
 import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
+const Statistics = ({all, good, neutral, bad}) => {
+  return (
+    <div>
+      {all === 0 ? 
+          <p>No feedback given</p> : 
+          <>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+          </>
+        }
+    </div>
+  );
+}
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  const handleGoodClick = () => {
+    setGood(g => g + 1)
+    setAll(a => a + 1)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const handleNeutralClick = () => {
+    setNeutral(n => n + 1)
+    setAll(a => a + 1)
   }
+
+  const handleBadClick = () => {
+    setBad(b => b + 1)
+    setAll(a => a + 1)
+  }
+
 
   return (
     <div>
-      {left}
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
 
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <h1>Statistics</h1>
+      <Statistics
+        all={all}
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
