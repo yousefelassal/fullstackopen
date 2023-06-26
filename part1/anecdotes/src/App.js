34a4d5746d78226d59bf68648b044403e761bfb9
Yousef <yousefsameh24@gmail.com>
2023-06-26 23:14:15 +0300

    solve 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,74 +1,29 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
-  )
-}
-
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  console.log(props)
-  let part1 = props.course.parts[0]
-  let part2 = props.course.parts[1]
-  let part3 = props.course.parts[2]
-
-  return (
-    <>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  let part1 = props.course.parts[0]
-  let part2 = props.course.parts[1]
-  let part3 = props.course.parts[2]
-  return (
-    <>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-    </>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>
+        Next anecdote
+      </button>
     </div>
   )
 }
