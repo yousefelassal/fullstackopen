e115a5100f3f008e634612b40278cf510b4ea969
Yousef <yousefsameh24@gmail.com>
2023-06-28 19:32:36 +0300

    refactor modules
    create components dir, export and import the component

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,10 @@
-const Note = ({ note }) => {
-  return (
-    <li>{note.content}</li>
-  )
-}
+import Note from './Components/Note'
 
 const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-
         {notes.map(note => 
           <Note key={note.id} note={note} />
         )}
