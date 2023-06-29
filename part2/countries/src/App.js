ca042d915c729a3977626b53809d62b0549206bc
Yousef <yousefsameh24@gmail.com>
2023-06-29 18:35:03 +0300

    move code

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,74 +1,54 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>
-  )
-}
+import { useState } from 'react'
+import Note from './Components/Note'
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll 
+    ? notes
+    : notes.filter(note => note.important)
 
 
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  )
-}
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
 
-const Content = (props) => {
-  console.log(props)
-  let part1 = props.course.parts[0]
-  let part2 = props.course.parts[1]
-  let part3 = props.course.parts[2]
-
-  return (
-    <>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  console.log(props)
-  let part1 = props.course.parts[0]
-  let part2 = props.course.parts[1]
-  let part3 = props.course.parts[2]
-  return (
-    <>
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
-    </>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
     </div>
   )
 }
