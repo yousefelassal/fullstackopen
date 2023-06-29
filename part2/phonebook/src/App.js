f2a1a2e257fc4fa305ece7fd81a86413afbd9456
Yousef <yousefsameh24@gmail.com>
2023-06-29 18:13:21 +0300

    filtering displayed elements

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,11 @@ import Note from './Components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+  const notesToShow = showAll 
+    ? notes
+    : notes.filter(note => note.important)
 
 
   const addNote = (event) => {
@@ -26,8 +31,13 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
