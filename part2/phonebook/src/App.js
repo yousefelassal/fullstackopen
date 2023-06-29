f46af14ca79fa3773ce12e4d89b1b7ec14b5d58f
Yousef <yousefsameh24@gmail.com>
2023-06-29 19:51:04 +0300

    solve 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPhone = (event) => {
     event.preventDefault()
@@ -14,10 +18,13 @@ const App = () => {
       return
     }
     const phoneObject = {
-      name: newName
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(phoneObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -31,12 +38,21 @@ const App = () => {
           />
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={e => setNewNumber(e.target.value)}
+           />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+
       )}
     </div>
   )
