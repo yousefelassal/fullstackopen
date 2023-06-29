ac82c9a872758eae43717213184be21eeb2cf8e5
Yousef <yousefsameh24@gmail.com>
2023-06-29 14:59:52 +0300

    solve 2.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,44 +1,4 @@
-const Header = ({name}) => <h2>{name}</h2>
-
-
-const Part = ({part, exercises}) => <>{part} {exercises} <br /></>
-
-
-const Content = ({parts}) => {
-  return (
-    <>
-      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-    </>
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <>
-      total of {parts.reduce((s, p) => s + p.exercises, 0)} exercises
-    </>
-  )
-}
-
-const CourseDetails = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
-
-const Course = ({ courses }) => {
-  console.log(courses)
-  return (
-    <>
-      <h1>Web development curriculum</h1>
-      {courses.map(course => <CourseDetails key={course.id} course={course} />)}
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
