1b4c58b14ff4a97c30c751eb41451d15137adfe8
Yousef <yousefsameh24@gmail.com>
2023-07-01 20:23:44 +0300

    adding styles

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Note from './Components/Note'
+import Notification from './Components/Notification'
 import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
@@ -48,9 +50,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
     }
@@ -58,6 +63,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
