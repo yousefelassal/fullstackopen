66105988834e0ed14eef757f0698565a9478f5be
Yousef <yousefsameh24@gmail.com>
2023-07-08 18:14:05 +0300

    fetching single resource

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -20,15 +20,24 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>Hello World awy!</h1>')
   })
   
   app.get('/api/notes', (request, response) => {
     response.json(notes)
   })
+
+  app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if(note){
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+    })
   
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
-  
