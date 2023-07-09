d93bef81f88dace6fd539c5446cd57e5e1288403
Yousef <yousefsameh24@gmail.com>
2023-07-09 18:07:54 +0300

    load deployed backend

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://black-rain-5614.fly.dev/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
