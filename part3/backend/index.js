8c3517123a352c2c7ef5ecc9707d6ad6b71acca7
Yousef <yousefsameh24@gmail.com>
2023-07-09 18:40:18 +0300

    transfer build to backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
