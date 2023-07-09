4e1221a4d5a93c5371a5693350b215ec2449a44e
Yousef <yousefsameh24@gmail.com>
2023-07-09 16:13:02 +0300

    load backend use CORS

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
