67cfa36006659c0056a23dd2965bb3a6993d29e5
Yousef <yousefsameh24@gmail.com>
2023-07-11 18:51:57 +0300

    validate backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -36,14 +36,12 @@ let notes = [
 ]
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important},
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -95,9 +93,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -109,6 +109,8 @@ const errorHandler = (error,request,response,next) => {
 
   if(error.name === 'CastError'){
     return response.status(400).send({error:'malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({error:error.message})
   }
   next(error)
 }
--- part3/backend/models/note.js
+++ part3/backend/models/note.js
@@ -14,10 +14,14 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
+  const noteSchema = new mongoose.Schema({
+    content: {
+      type: String,
+      minLength: 5,
+      required: true
+    },
+    important: Boolean
+  })
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
