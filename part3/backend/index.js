9182a4ebba7c0032f8e28fe0b28c4a5357d3b7d3
Yousef <yousefsameh24@gmail.com>
2023-07-11 15:18:58 +0300

    update order of middleware loading

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -14,7 +15,6 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-app.use(express.json())
 app.use(requestLogger)
 
 let notes = [
