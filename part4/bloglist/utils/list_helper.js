1b1d17c5f922ae69d9ceb4c9b09d58cdcc88969d
Yousef <yousefsameh24@gmail.com>
2023-07-13 15:04:16 +0300

    solve 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,14 @@
 const dummy = (blogs) => 1
 
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) =>
+    sum + item.likes
+    
+    return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
