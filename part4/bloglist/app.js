aa1a5b60f939469583e577fc8bcc58e32ce5f2c9
Yousef <yousefsameh24@gmail.com>
2023-07-18 20:46:04 +0300

    solve 4.23

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,11 +1,17 @@
+require('express-async-errors')
+const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 
 const app = express()
-const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -23,6 +29,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -22,12 +22,8 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  // token expires in 60*60 seconds, that is, in one hour
-  const token = jwt.sign(
-    userForToken,
-    process.env.SECRET,
-    { expiresIn: 60*60 }
-  )
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
   response
     .status(200)
     .send({ token, username: user.username, name: user.name })
