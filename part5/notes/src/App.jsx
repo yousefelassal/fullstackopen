866d9da06c34aa2bb9bd89543e8081d21c5f029b
Yousef <yousefsameh24@gmail.com>
2023-11-17 18:58:18 +0200

    Add useRef hook to noteFormRef

--- part5/notes/src/App.jsx
+++ part5/notes/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import Note from './components/Note'
 import Notification from './components/Notification'
@@ -17,6 +17,7 @@ const App = () => {
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const noteFormRef = useRef()
 
   useEffect(() => {
     noteService
@@ -105,7 +106,7 @@ const App = () => {
     )
 
     const noteForm = () => (
-      <Togglable buttonLabel='new note'>
+      <Togglable buttonLabel='new note' ref={noteFormRef}>
         <NoteForm createNote={addNote} />
       </Togglable>
     )
