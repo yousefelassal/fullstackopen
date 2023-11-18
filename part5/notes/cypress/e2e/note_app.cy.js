c01b8151e872af1022f13fd350bf9054a2e5baa2
Yousef <yousefsameh24@gmail.com>
2023-11-18 15:55:51 +0200

    add login test case

--- part5/notes/cypress/e2e/note_app.cy.js
+++ part5/notes/cypress/e2e/note_app.cy.js
@@ -11,4 +11,10 @@ describe('Note app', function() {
   it('login form can be opened', function() {
     cy.contains('login').click()
   })
+
+  it('user can login', function () {
+    cy.contains('login').click()
+    cy.get('input:first').type('admin')
+    cy.get('input:last').type('admin')
+  })  
 })
