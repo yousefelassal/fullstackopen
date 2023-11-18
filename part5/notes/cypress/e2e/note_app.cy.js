9919f2713c148b54c4f18c4a8280f6516bd042d5
Yousef <yousefsameh24@gmail.com>
2023-11-18 15:43:41 +0200

    Update note_app.cy.js with new front page content

--- part5/notes/cypress/e2e/note_app.cy.js
+++ part5/notes/cypress/e2e/note_app.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Note app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5173')
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
   })
 })
