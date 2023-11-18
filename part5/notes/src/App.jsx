90c816ff48de0a0686c59e150c87421265bc8c6d
Yousef <yousefsameh24@gmail.com>
2023-11-18 13:03:30 +0200

    Toggle visibility of note form

--- part5/notes/src/App.jsx
+++ part5/notes/src/App.jsx
@@ -37,6 +37,7 @@ const App = () => {
   }, [])
 
   const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility()
     noteService
       .create(noteObject)
       .then(returnedNote => {
