878cabeaf3799969edddb89467392370ab0e8068
Yousef <yousefsameh24@gmail.com>
2023-11-27 16:29:31 +0200

    Refactor noteReducer to handle new notes and toggle importance

--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -1,10 +1,21 @@
 const noteReducer = (state = [], action) => {
-    if (action.type === 'NEW_NOTE') {
-      state.concat(action.payload)
-      return state
+    switch(action.type) {
+      case 'NEW_NOTE':
+        return state.concat(action.payload)
+      case 'TOGGLE_IMPORTANCE': {
+        const id = action.payload.id
+        const noteToChange = state.find(n => n.id === id)
+        const changedNote = { 
+          ...noteToChange, 
+          important: !noteToChange.important 
+        }
+        return state.map(note =>
+          note.id !== id ? note : changedNote 
+        )
+       }
+      default:
+        return state
     }
-  
-    return state
 }
 
 export default noteReducer
