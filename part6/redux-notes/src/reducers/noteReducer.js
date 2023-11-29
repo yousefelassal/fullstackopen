86ca46a87299d05bf0e77836d51559fe971a4bd6
Yousef <yousefsameh24@gmail.com>
2023-11-29 16:09:03 +0200

    Refactor noteReducer initialState

--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -1,24 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
       const content = action.payload
