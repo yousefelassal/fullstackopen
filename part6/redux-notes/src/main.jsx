6e55d31454c71699381d82636ba0a7e85cca45db
Yousef <yousefsameh24@gmail.com>
2023-11-29 13:55:11 +0200

    Add note and filter reducers to main.jsx

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -4,6 +4,9 @@ import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
+
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
@@ -27,3 +30,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div />
   </Provider>
 )
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
