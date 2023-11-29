a6528f62590872974e2aae9a37a326c881f77e0a
Yousef <yousefsameh24@gmail.com>
2023-11-29 13:44:41 +0200

    Add filterChange action creator to filterReducer

--- part6/redux-notes/src/reducers/filterReducer.js
+++ part6/redux-notes/src/reducers/filterReducer.js
@@ -6,3 +6,12 @@ const filterReducer = (state = 'ALL', action) => {
         return state
     }
 }
+
+export const filterChange = (filter) => {
+    return {
+      type: 'SET_FILTER',
+      payload: { filter }
+    }
+}
+
+export default filterReducer
