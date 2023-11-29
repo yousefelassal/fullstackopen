c3c39abcd883c41180b7df2d27a92bb8c618afed
Yousef <yousefsameh24@gmail.com>
2023-11-29 13:40:37 +0200

    Add filter functionality to notes

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -2,10 +2,20 @@ import NewNote from "./components/NewNote"
 import Notes from "./components/Notes"
 
 const App = () => {
-
+  const filterSelected = (value) => {
+    console.log(value)
+  }
   return (
     <div>
       <NewNote />
+      <div>
+        all          <input type="radio" name="filter"
+          onChange={() => filterSelected('ALL')} />
+        important    <input type="radio" name="filter"
+          onChange={() => filterSelected('IMPORTANT')} />
+        nonimportant <input type="radio" name="filter"
+          onChange={() => filterSelected('NONIMPORTANT')} />
+      </div>
       <Notes />
     </div>
   )
