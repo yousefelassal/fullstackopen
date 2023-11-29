59e4658e365c27d9fc9de47a0cbc80fe1fd73d42
Yousef <yousefsameh24@gmail.com>
2023-11-29 16:19:45 +0200

    Refactor Redux notes initialization

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from 'react'
+
+import noteService from './services/notes'
+import { setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
 import NewNote from "./components/NewNote"
 import Notes from "./components/Notes"
 import VisibilityFilter from "./components/VisibilityFilter"
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
+  const dispatch = useDispatch()
+  useEffect(() => {
+    noteService
+      .getAll().then(notes => dispatch(setNotes(notes)))
+  }, [])
+  
   return (
     <div>
       <NewNote />
--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -4,8 +4,7 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import noteService from './services/notes'
-import noteReducer, { setNotes } from './reducers/noteReducer'
+import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
@@ -14,10 +13,6 @@ const store = configureStore({
     filter: filterReducer
   }
 })
-
-noteService.getAll().then(notes =>
-    store.dispatch(setNotes(notes))
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
