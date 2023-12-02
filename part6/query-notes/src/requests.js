b4868a3b12131dee6469609ca4e5e8ff9d6862a4
Yousef <yousefsameh24@gmail.com>
2023-12-02 14:41:14 +0200

    Update requests.js with base URL and add createNote function

--- part6/query-notes/src/requests.js
+++ part6/query-notes/src/requests.js
@@ -1,4 +1,9 @@
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:3001/notes'
+
 export const getNotes = () =>
-  axios.get('http://localhost:3001/notes').then(res => res.data)
+  axios.get(baseUrl).then(res => res.data)
+
+export const createNote = newNote =>
+    axios.post(baseUrl, newNote).then(res => res.data)
