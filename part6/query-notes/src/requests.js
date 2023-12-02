faa5d62d2cdf3af8d936fd43817319c261b7964c
Yousef <yousefsameh24@gmail.com>
2023-12-02 14:58:53 +0200

    Add updateNote function to requests.js

--- part6/query-notes/src/requests.js
+++ part6/query-notes/src/requests.js
@@ -6,4 +6,7 @@ export const getNotes = () =>
   axios.get(baseUrl).then(res => res.data)
 
 export const createNote = newNote =>
-    axios.post(baseUrl, newNote).then(res => res.data)
+  axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateNote = updatedNote =>
+  axios.put(`${baseUrl}/${updatedNote.id}`, updatedNote).then(res => res.data)
