7bca77be319185bd53dee9c3c3ba1d000a6a38ef
Yousef <yousefsameh24@gmail.com>
2023-12-02 15:10:07 +0200

    manually update set query on mutation success

--- part6/query-notes/src/App.jsx
+++ part6/query-notes/src/App.jsx
@@ -13,8 +13,9 @@ const App = () => {
 
   const newNoteMutation = useMutation({ 
     mutationFn: createNote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['notes'] })
+    onSuccess: (newNote) => {
+      const notes = queryClient.getQueryData(['notes'])
+      queryClient.setQueryData(['notes'], [...notes, newNote])
     }
   })
 
