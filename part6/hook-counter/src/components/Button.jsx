d940baedeb9bf8f397530fbb8c87cd5d38aa3331
Yousef <yousefsameh24@gmail.com>
2023-12-03 12:25:25 +0200

    Refactor Button component to use useCounterDispatch hook

--- part6/hook-counter/src/components/Button.jsx
+++ part6/hook-counter/src/components/Button.jsx
@@ -1,13 +1,13 @@
-import { useContext } from 'react'
-import CounterContext from '../CounterContext'
+import { useCounterDispatch } from '../CounterContext'
 
 const Button = ({ type, label }) => {
-    const [counter, dispatch] = useContext(CounterContext)
-    return (
-      <button onClick={() => dispatch({ type })}>
-        {label}
-      </button>
-    )
+
+  const dispatch = useCounterDispatch()
+  return (
+    <button onClick={() => dispatch({ type })}>
+      {label}
+    </button>
+  )
 }
 
 export default Button
