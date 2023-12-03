2ff48300fb3392984adb12b39d2cd3fc570da670
Yousef <yousefsameh24@gmail.com>
2023-12-03 12:21:41 +0200

    Refactor component imports and remove unused code

--- part6/hook-counter/src/App.jsx
+++ part6/hook-counter/src/App.jsx
@@ -1,48 +1,16 @@
-import { useReducer } from 'react'
-import { useContext } from 'react'
-import CounterContext from './CounterContext'
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-        return state + 1
-    case "DEC":
-        return state - 1
-    case "ZERO":
-        return 0
-    default:
-        return state
-  }
-}
-
-const Display = () => {
-  const [counter, dispatch] = useContext(CounterContext)
-  return <div>{counter}</div>
-}
-
-const Button = ({ type, label }) => {
-  const [counter, dispatch] = useContext(CounterContext)
-  return (
-    <button onClick={() => dispatch({ type })}>
-      {label}
-    </button>
-  )
-}
+import Display from './components/Display'
+import Button from './components/Button'
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-
+    <div>
       <Display />
       <div>
-
         <Button type='INC' label='+' />
         <Button type='DEC' label='-' />
         <Button type='ZERO' label='0' />
       </div>
-    </CounterContext.Provider>
+    </div>
   )
 }
 
