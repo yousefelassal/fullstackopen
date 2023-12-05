2f1f35503c5cbf73b89ce9d205d38749fb331e68
Yousef <yousefsameh24@gmail.com>
2023-12-05 17:01:09 +0200

    Refactor routing in App component

--- part7/router/src/App.jsx
+++ part7/router/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState }  from 'react'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const Home = () => (
   <div> <h2>TKTL notes app</h2> </div>
@@ -13,43 +16,29 @@ const Users = () => (
 )
 
 const App = () => {
-  const [page, setPage] = useState('home')
-
-  const toPage = (page) => (event) => {
-    event.preventDefault()
-    setPage(page)
-  }
-
-  const content = () => {
-    if (page === 'home') {
-      return <Home />
-    } else if (page === 'notes') {
-      return <Notes />
-    } else if (page === 'users') {
-      return <Users />
-    }
-  }
 
   const padding = {
     padding: 5
   }
 
   return (
-    <div>
+    <Router>
       <div>
-        <a href="" onClick={toPage('home')} style={padding}>
-          home
-        </a>
-        <a href="" onClick={toPage('notes')} style={padding}>
-          notes
-        </a>
-        <a href="" onClick={toPage('users')} style={padding}>
-          users
-        </a>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
       </div>
 
-      {content()}
-    </div>
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2023</i>
+      </div>
+    </Router>
   )
 }
 
