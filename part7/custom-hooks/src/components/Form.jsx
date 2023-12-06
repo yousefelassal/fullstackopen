0560c1dfced8c8e357ec2bd3685fae345191b7a5
Yousef <yousefsameh24@gmail.com>
2023-12-06 17:05:22 +0200

    Add Form component with custom hooks

---
+++ part7/custom-hooks/src/components/Form.jsx
@@ -0,0 +1,27 @@
+import useField from "../use-field"
+
+const Form = () => {
+    const name = useField('text')
+  const born = useField('date')
+  const height = useField('number')
+
+  return (
+    <div>
+      <form>
+        name: 
+        <input  {...name} /> 
+        <br/> 
+        birthdate:
+        <input {...born} />
+        <br /> 
+        height:
+        <input {...height} />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value}
+      </div>
+    </div>
+  )
+}
+
+export default Form
