4f820debe77eadee02d0d02aba841ddb39d41a04
Yousef <yousefsameh24@gmail.com>
2023-12-07 10:12:11 +0200

    solve 7.8

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,10 +18,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const fetchResources = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    fetchResources()
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource)
+      .then(response => {
+        setResources(resources.concat(response.data))
+    })
   }
 
   const service = {
