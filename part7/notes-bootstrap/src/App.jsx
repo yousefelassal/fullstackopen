9074e61c8d9146597b0e42aa3a9f6ae9aecb6c87
Yousef <yousefsameh24@gmail.com>
2023-12-07 10:57:36 +0200

    Add table layout for notes

--- part7/notes-bootstrap/src/App.jsx
+++ part7/notes-bootstrap/src/App.jsx
@@ -10,6 +10,7 @@ import {
   useMatch
 } from "react-router-dom"
 
+import Table from 'react-bootstrap/Table'
 
 const Home = () => (
   <div>
@@ -31,13 +32,23 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>
+                {note.content}
+              </Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
