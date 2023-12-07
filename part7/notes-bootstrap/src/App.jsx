51b11647a251e9a1e03f03da251333a843f2ed0a
Yousef <yousefsameh24@gmail.com>
2023-12-07 10:56:09 +0200

    Refactor notes app components

--- part7/notes-bootstrap/src/App.jsx
+++ part7/notes-bootstrap/src/App.jsx
@@ -1,177 +1,142 @@
 import { useState } from 'react'
+
 import {
   Routes,
   Route,
   Link,
+  Navigate,
+  useParams,
   useNavigate,
   useMatch
 } from "react-router-dom"
 
-import { useField } from './hooks'
 
-const Menu = () => {
-  const padding = {
-    paddingRight: 5
-  }
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  </div>
+)
+
+const Note = ({ note }) => {
   return (
     <div>
-      <Link to="/" style={padding}>anecdotes</Link>
-      <Link to="create" style={padding}>create new</Link>
-      <Link to="about" style={padding}>about</Link>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
     </div>
   )
 }
 
-const AnecdoteList = ({ anecdotes }) => (
+const Notes = ({ notes }) => (
   <div>
-    <h2>Anecdotes</h2>
+    <h2>Notes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id}><Link to={`/anecdotes/${anecdote.id}`} >{anecdote.content}</Link></li>)}
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
 
-const About = () => (
+const Users = () => (
   <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
   </div>
 )
 
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
+const Login = (props) => {
+  const navigate = useNavigate()
 
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
-  </div>
-)
-
-const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    props.addNew({
-      content: content.getFieldProps().value,
-      author: author.getFieldProps().value,
-      info: info.getFieldProps().value,
-      votes: 0
-    })
-  }
-
-  const handleReset = (e) => {
-    e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+  const onSubmit = (event) => {
+    event.preventDefault()
+    props.onLogin('mluukkai')
+    navigate('/')
   }
 
   return (
     <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
         <div>
-          content
-          <input name='content' {...content.getFieldProps()} />
+          username: <input />
         </div>
         <div>
-          author
-          <input name='author' {...author.getFieldProps()} />
+          password: <input type='password' />
         </div>
-        <div>
-          url for more info
-          <input name='info' {...info.getFieldProps()} />
-        </div>
-        <button>create</button>
-        <button onClick={handleReset}>reset</button>
+        <button type="submit">login</button>
       </form>
-    </div>
-  )
-
-}
-
-const Anecdote = ({ anecdote }) => {
-  return (
-    <div>
-      <h2>{anecdote.content}</h2>
-      <div>has {anecdote.votes} votes</div>
-      <br />
-      <div>for more info see <a href={anecdote.info}>{anecdote.info}</a></div>
-      <br />
     </div>
   )
 }
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
+  const [notes, setNotes] = useState([
     {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
-      votes: 0,
-      id: 1
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
     },
     {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
-      votes: 0,
-      id: 2
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
     }
   ])
 
-  const navigate = useNavigate();
+  const [user, setUser] = useState(null)
 
-  const [notification, setNotification] = useState('')
-
-  const addNew = (anecdote) => {
-    anecdote.id = Math.round(Math.random() * 10000)
-    setAnecdotes(anecdotes.concat(anecdote))
-    navigate(`/anecdotes/${anecdote.id}`)
-    setNotification(`a new anecdote ${anecdote.content} created!`)
-    setTimeout(() => setNotification(''), 10000)
+  const login = (user) => {
+    setUser(user)
   }
 
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
-
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
-
-    const voted = {
-      ...anecdote,
-      votes: anecdote.votes + 1
-    }
-
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+  const padding = {
+    padding: 5
   }
 
-  const match = useMatch('anecdotes/:id')
-  const anecdote = match
-    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+  const match = useMatch('/notes/:id')
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
     : null
 
   return (
     <div className="container">
-      <h1>Software anecdotes</h1>
-      <Menu />
-      {notification}
-      <Routes>
-        <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
-        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<CreateNew addNew={addNew} />} />
-      </Routes>
-      <Footer />
+        <div>
+          <Link style={padding} to="/">home</Link>
+          <Link style={padding} to="/notes">notes</Link>
+          <Link style={padding} to="/users">users</Link>
+          {user
+            ? <em>{user} logged in</em>
+            : <Link style={padding} to="/login">login</Link>
+          }
+        </div>
+
+        <Routes>
+          <Route path="/notes/:id" element={<Note note={note} />} />
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <div>
+        <br />
+        <em>Note app, Department of Computer Science 2023</em>
+      </div>
     </div>
   )
 }
