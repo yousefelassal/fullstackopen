a5a7c228984e2310caf14eec87b0ae9509777fa4
Yousef <yousefsameh24@gmail.com>
2023-12-08 14:37:11 +0200

    update babel presents

--- part7/webpack/webpack.config.js
+++ part7/webpack/webpack.config.js
@@ -13,7 +13,7 @@ const config = () => {
           test: /\.js$/,
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-react'],
+            presets: ['@babel/present-env', '@babel/preset-react'],
           },
         },
       ],
