c0b6f99e088868c401523e17ae0f55147b9c67f2
Yousef <yousefsameh24@gmail.com>
2023-12-08 15:18:12 +0200

    Refactor App component to add a click counter and values array

--- part7/webpack/src/App.js
+++ part7/webpack/src/App.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
-import './index.css'
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
-
-  return (
-    <div className="container">
-      hello webpack {counter} clicks
-      <button onClick={() => setCounter(counter + 1)}>
-        press
-      </button>
-    </div>
-  )
-}
-
-export default App
+    const [counter, setCounter] = useState(0)
+    const [values, setValues] = useState()
+  
+    const handleClick = () => {
+      setCounter(counter + 1)
+      setValues(values.concat(counter))
+    }
+  
+    return (
+      <div className="container">
+        hello webpack {counter} clicks
+        <button onClick={handleClick}>
+          press
+        </button>
+      </div>
+    )
+  }
