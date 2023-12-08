394eb0ed5f15341b222580474ac273d0bf6da558
Yousef <yousefsameh24@gmail.com>
2023-12-08 15:48:00 +0200

    update configs, useNotes in app, install axios

--- part7/webpack/webpack.config.js
+++ part7/webpack/webpack.config.js
@@ -1,6 +1,14 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = () => {
+const config = (env, argv) => {
+  console.log('argv', argv.mode)
+
+
+  const backend_url = argv.mode === 'production'
+    ? 'https://notes2023.fly.dev/api/notes'
+    : 'http://localhost:3001/notes'
+
   return {
     entry: './src/index.js',
     output: {
@@ -8,9 +16,9 @@ const config = () => {
       filename: 'main.js'
     },
     devServer: {
-        static: path.resolve(__dirname, 'build'),
-        compress: true,
-        port: 3000,
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
     },
     devtool: 'source-map',
     module: {
@@ -19,15 +27,20 @@ const config = () => {
           test: /\.js$/,
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
           },
         },
         {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
         },
       ],
     },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
+    ]
   }
 }
 
