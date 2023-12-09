e7859137efc2e912eee02972d5ca8d17eb41ec2e
Yousef <yousefsameh24@gmail.com>
2023-12-09 13:20:52 +0200

    Add state and render current anecdote

--- part7/anecdotes-class-component/src/App.jsx
+++ part7/anecdotes-class-component/src/App.jsx
@@ -3,12 +3,25 @@ import React from 'react'
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      anecdotes: [],
+      current: 0
+    }
   }
 
   render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>
+    }
+
     return (
       <div>
         <h1>anecdote of the day</h1>
+        <div>
+          {this.state.anecdotes[this.state.current].content}
+        </div>
+        <button>next</button>
       </div>
     )
   }
