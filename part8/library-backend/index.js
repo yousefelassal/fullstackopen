3914640efff59988381145657a06d4de0867eec0
Yousef <yousefsameh24@gmail.com>
2023-12-16 17:37:58 +0200

    solve 8.5

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,10 +126,16 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+        if (args.author && args.genre) {
+            return books.filter(b => b.author === args.author && b.genres.includes(args.genre))
+        }
+        if (args.author) {
+            return books.filter(b => b.author === args.author)
+        }
+        if (args.genre) {
+            return books.filter(b => b.genres.includes(args.genre))
+        }
         return books
-      }
-      return books.filter(b => b.author === args.author)
     },
     allAuthors: () => authors
   },
