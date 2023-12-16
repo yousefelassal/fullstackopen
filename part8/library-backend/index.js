8f566b6ec60897989c32f32cf7cf026d309e7c5e
Yousef <yousefsameh24@gmail.com>
2023-12-16 17:46:57 +0200

    solve 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -128,6 +128,7 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
@@ -163,6 +164,15 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
