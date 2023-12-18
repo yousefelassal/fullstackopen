b6a78df5bcd349960bf13625a896640e20f48dc3
Yousef <yousefsameh24@gmail.com>
2023-12-18 09:33:51 +0200

    connect apollo client

--- part8/phonebook/src/main.jsx
+++ part8/phonebook/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allPersons  {
+      name,
+      phone,
+      address {
+        street,
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
