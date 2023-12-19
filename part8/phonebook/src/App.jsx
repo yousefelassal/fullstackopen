b6b3fc9e5c07fe23a523aba8123270bcc5dd4132
Yousef <yousefsameh24@gmail.com>
2023-12-19 17:51:51 +0200

    Add Apollo Client and fetch all persons

--- part8/phonebook/src/App.jsx
+++ part8/phonebook/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from '@apollo/client'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ALL_PERSONS = gql`
+query {
+  allPersons {
+    name
+    phone
+    id
+  }
+}
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {result.data.allPersons.map(p => p.name).join(', ')}
+    </div>
   )
 }
 
