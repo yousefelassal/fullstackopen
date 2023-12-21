638a86a7d7ad7e3944f5b64761055d3f793a0f92
Yousef <yousefsameh24@gmail.com>
2023-12-21 10:56:25 +0200

    Add error notification and handling in App component

--- part8/phonebook/src/App.jsx
+++ part8/phonebook/src/App.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000
   })
@@ -12,11 +15,30 @@ const App = () => {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons}/>
-      <PersonForm />
+      <PersonForm setError={notify} />
     </>
+  )
+}
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
+    </div>
   )
 }
 
