cda42baf1e9b9a144feee3cc264f424a009d5787
Yousef <yousefsameh24@gmail.com>
2023-12-23 15:50:35 +0200

    Add me resolver to GraphQL API

--- part8/phonebook-backend/index.js
+++ part8/phonebook-backend/index.js
@@ -85,7 +85,10 @@ const resolvers = {
 
       return Person.find({ phone: { $exists: args.phone === 'YES' } })
     },
-    findPerson: async (root, args) => Person.findOne({ name: args.name })
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => {
+      return context.currentUser
+    }
   },
   Person: {
     address: root => {
@@ -169,6 +172,17 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User
+        .findById(decodedToken.id).populate('friends')
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
