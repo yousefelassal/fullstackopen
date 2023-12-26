b3371cf34ddf28948c7c5886ece1a5ff63cf2d6c
Yousef <yousefsameh24@gmail.com>
2023-12-26 14:12:55 +0200

    Add authentication to ApolloClient

--- part8/phonebook/src/main.jsx
+++ part8/phonebook/src/main.jsx
@@ -5,11 +5,27 @@ import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
+  createHttpLink,
 } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('phonenumbers-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  }
+})
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
