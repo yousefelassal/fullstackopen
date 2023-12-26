a91c5a4e1bdb546da26c093c38bd1060f97bbfa5
Yousef <yousefsameh24@gmail.com>
2023-12-26 10:25:07 +0200

    Add login functionality to App component

--- part8/phonebook/src/App.jsx
+++ part8/phonebook/src/App.jsx
@@ -4,13 +4,28 @@ import { useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000
   })
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm 
+          setToken={setToken}
+          setError={setErrorMessage}
+        />
+      </div>
+    )
+  }
 
   if (result.loading)  {
     return <div>loading...</div>
