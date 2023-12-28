7caf63111160d220f26e5168cf049f2ec90ee8c3
Yousef <yousefsameh24@gmail.com>
2023-12-28 16:29:04 +0200

    Add WebSocket server and GraphQL WebSocket support

--- part8/phonebook-backend/index.js
+++ part8/phonebook-backend/index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require('@apollo/server')
 const { expressMiddleware } = require('@apollo/server/express4')
 const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
+const { WebSocketServer } = require('ws')
+const { useServer } = require('graphql-ws/lib/use/ws')
 const express = require('express')
 const cors = require('cors')
 const http = require('http')
@@ -34,9 +36,25 @@ const start = async () => {
   const app = express()
   const httpServer = http.createServer(app)
 
+  const wsServer = new WebSocketServer({ 
+    server: httpServer,
+    path: '/',
+  })
+
   const server = new ApolloServer({
     schema: makeExecutableSchema({ typeDefs, resolvers }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup().dispose();
+            }
+          }
+        }
+      }
+    ],
   })
 
   await server.start()
