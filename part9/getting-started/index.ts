3d526f30c484f342439dc21c9a444955462b42cb
Yousef <yousefsameh24@gmail.com>
2024-01-16 19:17:56 +0200

    Refactor calculator function in index.ts

--- part9/getting-started/index.ts
+++ part9/getting-started/index.ts
@@ -6,13 +6,11 @@ const calculator = (a: number, b: number, op: Operation) : number => {
     case 'multiply':
       return a * b;
     case 'divide':
-
       if (b === 0) throw new Error('Can\'t divide by 0!');
       return a / b;
     case 'add':
       return a + b;
     default:
-
       throw new Error('Operation is not multiply, add or divide!');
   }
 }
