107e481a8a0cf6af9412ba4b07e3e2e65863b157
Yousef <yousefsameh24@gmail.com>
2024-01-16 19:17:09 +0200

    Refactor calculator function and add error handling

--- part9/getting-started/index.ts
+++ part9/getting-started/index.ts
@@ -1,5 +1,28 @@
-const multiplicator = (a:number, b:number, printText:string) => {
-    console.log(printText,  a * b);
+type Operation = 'multiply' | 'add' | 'divide';
+
+
+const calculator = (a: number, b: number, op: Operation) : number => {
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+
+      throw new Error('Operation is not multiply, add or divide!');
   }
-  
-multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+}
+
+try {
+  console.log(calculator(1, 5 , 'divide'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
