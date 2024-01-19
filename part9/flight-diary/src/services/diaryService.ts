c9e3950130917a087fc8b49f5fbea22350606a56
Yousef <yousefsameh24@gmail.com>
2024-01-19 14:33:55 +0200

    create service

---
+++ part9/flight-diary/src/services/diaryService.ts
@@ -0,0 +1,14 @@
+import diaryData from '../../data/entries.json';
+
+const getEntries = () => {
+  return diaryData;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary
+};
