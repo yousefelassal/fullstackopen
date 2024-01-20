19c765080f1bc9755d0130a4254ccca40d899540
Yousef <yousefsameh24@gmail.com>
2024-01-20 17:54:47 +0200

    Add route to get diary by ID

--- part9/flight-diary/src/routes/diaries.ts
+++ part9/flight-diary/src/routes/diaries.ts
@@ -7,6 +7,16 @@ router.get('/', (_req, res) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 router.post('/', (_req, res) => {
   res.send('Saving a diary!');
 });
