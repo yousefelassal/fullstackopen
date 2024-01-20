9a5904786177d54a4b12a8e933a3acc91c94445c
Yousef <yousefsameh24@gmail.com>
2024-01-20 14:08:51 +0200

    Add cors package and configure CORS middleware

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import diagnosesRouter from './routes/diagnoses';
 import patientRouter from './routes/patients';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors()); // eslint-disable-line @typescript-eslint/no-unsafe-call
 
 const PORT = 3001;
 
