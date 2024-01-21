296bbbec1064c2c03e82af36aa6573c9ec9c53d1
Yousef <yousefsameh24@gmail.com>
2024-01-21 11:49:53 +0200

    Refactor parseComment function and remove unused code

--- part9/flight-diary/src/utils.ts
+++ part9/flight-diary/src/utils.ts
@@ -1,15 +1,27 @@
 import { NewDiaryEntry } from './types';
 
+const isString = (text: unknown): text is string => {
+    return typeof text === 'string' || text instanceof String;
+};
+
+const parseComment = (comment: unknown): string => {
+    if (!comment || !isString(comment)) {
+      throw new Error('Incorrect or missing comment');
+    }  
+    return comment;
+};
+
+
 const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
-    console.log(object); // now object is no longer unused
-    const newEntry: NewDiaryEntry = {
-      weather: 'cloudy', // fake the return value
-      visibility: 'great',
-      date: '2022-1-1',
-      comment: 'fake news'
-    };
-   
-    return newEntry;
-   };
+ console.log(object); // now object is no longer unused
+ const newEntry: NewDiaryEntry = {
+   weather: 'cloudy', // fake the return value
+   visibility: 'great',
+   date: '2022-1-1',
+   comment: 'fake news'
+ };
+
+ return newEntry;
+};
 
 export default toNewDiaryEntry;
