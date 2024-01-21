8423fddaa3cd5f4862380b947e02c263fbbb5b39
Yousef <yousefsameh24@gmail.com>
2024-01-21 12:06:46 +0200

    Refactor diaryData to diaryEntries

--- part9/flight-diary/data/diaries.ts
+++ part9/flight-diary/data/diaries.ts
@@ -1,6 +1,7 @@
 import { DiaryEntry } from "../src/types";
+import toNewDiaryEntry from "../src/utils";
 
-const diaryData: DiaryEntry[] = [
+const data = [
     {
         "id": 1,
         "date": "2017-01-01",
@@ -31,4 +32,10 @@ const diaryData: DiaryEntry[] = [
     }
 ];
 
-export default diaryData;
+const diaryEntries: DiaryEntry [] = data.map(obj => {
+    const object = toNewDiaryEntry(obj) as DiaryEntry;
+    object.id = obj.id;
+    return object;
+  });
+  
+  export default diaryEntries;
