681bda91d540a28f115f09571bf26ad001008fbb
Yousef <yousefsameh24@gmail.com>
2024-01-21 12:02:50 +0200

    Refactor Weather type to use enum

--- part9/flight-diary/src/types.ts
+++ part9/flight-diary/src/types.ts
@@ -1,4 +1,10 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+export enum Weather {
+    Sunny = 'sunny',
+    Rainy = 'rainy',
+    Cloudy = 'cloudy',
+    Stormy = 'stormy',
+    Windy = 'windy',
+}
 
 export type Visibility = 'great' | 'good' | 'ok' | 'poor';
 
