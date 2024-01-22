af256d56aac7e024b6ea0a3c6c3819017db11330
Yousef <yousefsameh24@gmail.com>
2024-01-22 14:08:59 +0200

    Add note creation functionality

--- part9/typed-notes/src/App.tsx
+++ part9/typed-notes/src/App.tsx
@@ -7,9 +7,37 @@ interface Note {
 
 const App = () => {
   const [newNote, setNewNote] = useState('');
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[]>([
+    {id: 1, content: 'HTML is easy'},
+  ]);
 
-  return null
+  const noteCreation = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <form onSubmit={noteCreation}>
+        <input 
+          value={newNote}
+          onChange={e => setNewNote(e.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>{note.content}</li>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
