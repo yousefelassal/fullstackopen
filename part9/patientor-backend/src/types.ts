09f9937848592e58f62f1bda6d0a3566f051040e
Yousef <yousefsameh24@gmail.com>
2024-01-23 18:42:40 +0200

    Add HealthCheckEntry interface and update Entry type

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,6 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+  
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+
 
 export enum Gender {
     Male = 'male',
@@ -23,6 +42,11 @@ export interface Patient {
     occupation: string;
     entries: Entry[];
 }
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
