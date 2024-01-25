66d8c5f4014f0b2fadd2a167cb86b71a0257865e
Yousef <yousefsameh24@gmail.com>
2024-01-25 21:50:56 +0200

    Update styles and add PressableText component

--- partie10/rate-repository-app/App.js
+++ partie10/rate-repository-app/App.js
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, TextInput, Text, View } from 'react-native';
+import PressableText from './src/PressableText';
 
 export default function App() {
   const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text className="text-4xl text-white">Hello World!</Text>
+      <PressableText />
       <TextInput
         style={{height: 40}}
         placeholder="Type here to translate!"
         onChangeText={newText => setText(newText)}
         defaultValue={text}
+        className="border rounded-lg bg-white/40 text-white"
       />
       <Text style={{padding: 10, fontSize: 42}}>
         {text
@@ -27,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#555',
     alignItems: 'center',
     justifyContent: 'center',
   },
