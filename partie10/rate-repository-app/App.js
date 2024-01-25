7fdb79b02452f1e34689faf588d6cf31a5e67536
Yousef <yousefsameh24@gmail.com>
2024-01-25 16:50:09 +0200

    Add text input and display pizza emojis based on input text

--- partie10/rate-repository-app/App.js
+++ partie10/rate-repository-app/App.js
@@ -1,10 +1,24 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
