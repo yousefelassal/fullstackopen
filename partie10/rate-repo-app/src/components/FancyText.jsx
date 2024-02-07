e8af5e982e7774a815d6d3472e11135b78bd976c
Yousef <yousefsameh24@gmail.com>
2024-02-07 18:14:33 +0200

    Add FancyText component to MainText

---
+++ partie10/rate-repo-app/src/components/FancyText.jsx
@@ -0,0 +1,40 @@
+import { Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'grey',
+    fontSize: 14,
+  },
+  blueText: {
+    color: 'blue',
+  },
+  bigText: {
+    fontSize: 24,
+    fontWeight: '700',
+  },
+});
+
+const FancyText = ({ isBlue, isBig, children }) => {
+  const textStyles = [
+    styles.text,
+    isBlue && styles.blueText,
+    isBig && styles.bigText,
+  ];
+
+  return <Text style={textStyles}>{children}</Text>;
+};
+
+const MainText = () => {
+  return (
+    <>
+      <FancyText>Simple text</FancyText>
+      <FancyText isBlue>Blue text</FancyText>
+      <FancyText isBig>Big text</FancyText>
+      <FancyText isBig isBlue>
+        Big blue text
+      </FancyText>
+    </>
+  );
+};
+
+export default MainText;
