415a80ed87a7bcd7e539e5335ccfd76718388f4a
Yousef <yousefsameh24@gmail.com>
2024-02-08 13:54:29 +0200

    Add react-router-native for routing

--- partie10/rate-repo-app/src/components/Main.jsx
+++ partie10/rate-repo-app/src/components/Main.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
 import RepositoryList from './RepoList';
-import Text from './Text';
 import AppBar from './AppBar';
 
 const styles = StyleSheet.create({
@@ -15,9 +15,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <RepositoryList />
-      </View>
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
