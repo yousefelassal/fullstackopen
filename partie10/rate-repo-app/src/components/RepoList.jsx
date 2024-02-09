cad54c718e15571903ac6f7ec3a8309e72318c4b
Yousef <yousefsameh24@gmail.com>
2024-02-09 20:56:54 +0200

    Refactor RepositoryList component to use custom hook

--- partie10/rate-repo-app/src/components/RepoList.jsx
+++ partie10/rate-repo-app/src/components/RepoList.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepoItem from './RepoItem';
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -11,19 +11,7 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
-  const [repositories, setRepositories] = useState();
-
-  const fetchRepositories = async () => {
-    const response = await fetch('http://127.0.0.1:5000/api/repositories')
-    const json = await response.json()
-
-    console.log(json)
-    setRepositories(json)
-  }
-
-  useEffect(() => {
-    fetchRepositories()
-  }, [])
+  const { repositories } = useRepositories();
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
