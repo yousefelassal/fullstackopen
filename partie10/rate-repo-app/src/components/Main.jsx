18b310060381ccbe76d6ddbbaba6d4586c2dfc8e
Yousef <yousefsameh24@gmail.com>
2024-02-09 14:38:41 +0200

    Add BMI calculator route

--- partie10/rate-repo-app/src/components/Main.jsx
+++ partie10/rate-repo-app/src/components/Main.jsx
@@ -20,6 +20,7 @@ const Main = () => {
       <Routes>
         <Route path="/" element={<RepositoryList />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/bmi" element={<BodyMassIndexCalculator />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </View>
