bb99f11c205290cbe8c306ab90316db28d746b10
Yousef <yousefsameh24@gmail.com>
2024-02-10 17:42:14 +0200

    create shopping cart storage

---
+++ partie10/rate-repo-app/src/utils/shoppingCart.js
@@ -0,0 +1,49 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class ShoppingCartStorage {
+  constructor(namespace = 'shoppingCart') {
+    this.namespace = namespace;
+  }
+
+  async getProducts() {
+    const rawProducts = await AsyncStorage.getItem(
+      `${this.namespace}:products`,
+    );
+
+    return rawProducts ? JSON.parse(rawProducts) : [];
+  }
+
+  async addProduct(productId) {
+    const currentProducts = await this.getProducts();
+    const newProducts = [...currentProducts, productId];
+
+    await AsyncStorage.setItem(
+      `${this.namespace}:products`,
+      JSON.stringify(newProducts),
+    );
+  }
+
+  async clearProducts() {
+    await AsyncStorage.removeItem(`${this.namespace}:products`);
+  }
+}
+
+const doShopping = async () => {
+  const shoppingCartA = new ShoppingCartStorage('shoppingCartA');
+  const shoppingCartB = new ShoppingCartStorage('shoppingCartB');
+
+  await shoppingCartA.addProduct('chips');
+  await shoppingCartA.addProduct('soda');
+
+  await shoppingCartB.addProduct('milk');
+
+  const productsA = await shoppingCartA.getProducts();
+  const productsB = await shoppingCartB.getProducts();
+
+  console.log(productsA, productsB);
+
+  await shoppingCartA.clearProducts();
+  await shoppingCartB.clearProducts();
+};
+
+doShopping();
