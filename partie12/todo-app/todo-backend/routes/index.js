92a8ac93cc80d78966d85665c757f7934b519991
Yousef <yousefsameh24@gmail.com>
2024-03-24 16:49:15 +0200

    solve 12.9

--- partie12/todo-app/todo-backend/routes/index.js
+++ partie12/todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
@@ -11,6 +12,7 @@ router.get('/', async (req, res) => {
 
   res.send({
     ...configs,
+    redis,
     visits
   });
 });
