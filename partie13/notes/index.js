abf5dc3806ea4cb662bbd434edd7667c46f8abad
Yousef <yousefsameh24@gmail.com>
2024-04-13 13:40:47 +0200

    Refactor index.js to add an API endpoint for fetching notes from the database

--- partie13/notes/index.js
+++ partie13/notes/index.js
@@ -1,18 +1,15 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.')
-    const notes = await sequelize.query('SELECT * FROM notes', { type: QueryTypes.SELECT })
-    console.log(notes)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+app.get('/api/notes', async (req, res) => {
+  const notes = await sequelize.query('SELECT * FROM notes', { type: QueryTypes.SELECT })
+  res.json(notes)
+})
 
-main()
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
