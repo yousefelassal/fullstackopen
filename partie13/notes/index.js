7f83cabb080360dd158a1db0f5b2a30d643a6a75
Yousef <yousefsameh24@gmail.com>
2024-04-13 15:55:19 +0200

    Refactor index.js to add Note model and API endpoint for fetching notes from the database

--- partie13/notes/index.js
+++ partie13/notes/index.js
@@ -1,12 +1,36 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
+class Note extends Model {}
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
+
 app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query('SELECT * FROM notes', { type: QueryTypes.SELECT })
+  const notes = await Note.findAll()
   res.json(notes)
 })
 
