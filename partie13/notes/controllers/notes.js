402d5611557e00c2488637900adc762acf308b86
Yousef <yousefsameh24@gmail.com>
2024-04-15 16:27:25 +0200

    create notes controller

---
+++ partie13/notes/controllers/notes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router()
+
+const { Note } = require('../models')
+
+router.get('/', async (req, res) => {
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const note = await Note.create(req.body)
+    res.json(note)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    await note.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+      await note.update(req.body)
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
+  } catch(e) {
+    res.status(400).json({ error: e.message })
+  }
+})
+
+module.exports = router
