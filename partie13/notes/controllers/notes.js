3276c9ecdad4d472a4e1dd064e9e21324550e708
Yousef <yousefsameh24@gmail.com>
2024-04-15 16:52:52 +0200

    Refactor note routes in notes.js: add middleware to search for note

--- partie13/notes/controllers/notes.js
+++ partie13/notes/controllers/notes.js
@@ -16,34 +16,33 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    res.json(note)
+const noteFinder = async (req, res, next) => {
+  req.note = await Note.findByPk(req.params.id)
+  next()
+}
+
+router.get('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    res.json(req.note)
   } else {
     res.status(404).end()
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  const note = await Note.findByPk(req.params.id)
-  if (note) {
-    await note.destroy()
+router.delete('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    await req.note.destroy()
   }
   res.status(204).end()
 })
 
-router.put('/:id', async (req, res) => {
-  try {
-    const note = await Note.findByPk(req.params.id)
-    if (note) {
-      await note.update(req.body)
-      res.json(note)
-    } else {
-      res.status(404).end()
-    }
-  } catch(e) {
-    res.status(400).json({ error: e.message })
+router.put('/:id', noteFinder, async (req, res) => {
+  if (req.note) {
+    req.note.important = req.body.important
+    await req.note.save()
+    res.json(req.note)
+  } else {
+    res.status(404).end()
   }
 })
 
