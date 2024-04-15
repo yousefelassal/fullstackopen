361be811d802b2cd6f075b5dc0208aa313c7f59c
Yousef <yousefsameh24@gmail.com>
2024-04-15 14:46:38 +0200

    Add GET endpoint for retrieving a specific note by ID in index.js

--- partie13/notes/index.js
+++ partie13/notes/index.js
@@ -54,6 +54,15 @@ app.post('/api/notes', async (req, res) => {
   }
 })
 
+app.get('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
