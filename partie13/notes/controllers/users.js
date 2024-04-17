e104604d50a129efdd20fe729e6d5b08425b6d81
Yousef <yousefsameh24@gmail.com>
2024-04-17 13:29:50 +0200

    Update users controller to include Note model in findAll query

--- partie13/notes/controllers/users.js
+++ partie13/notes/controllers/users.js
@@ -1,9 +1,14 @@
 const router = require('express').Router()
 
 const { User } = require('../models')
+const { Note } = require('../models')
 
 router.get('/', async (req, res) => {
-  const users = await User.findAll()
+  const users = await User.findAll({
+    include: {
+      model: Note,
+    }
+  })
   res.json(users)
 })
 
