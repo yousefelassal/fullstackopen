229de62e4a35d0606bc330b91059d821b86c40be
Yousef <yousefsameh24@gmail.com>
2024-04-17 13:58:40 +0200

    Add tokenExtractor middleware to handle authentication in notes.js controller

--- partie13/notes/controllers/notes.js
+++ partie13/notes/controllers/notes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+const { SECRET } = require('../util/config')
 
 const { Note } = require('../models')
 const { User } = require('../models')
@@ -8,10 +11,24 @@ router.get('/', async (req, res) => {
   res.json(notes)
 })
 
-router.post('/', async (req, res) => {
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch{
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+router.post('/', tokenExtractor, async (req, res) => {
   try {
-    const user = await User.findOne()
-    const note = await Note.create({ ...req.body, userId: user.id })
+    const user = await User.findByPk(req.decodedToken.id)
+    const note = await Note.create({ ...req.body, userId: user.id, date: new Date() })
     res.json(note)
   } catch(error) {
     return res.status(400).json({ error })
