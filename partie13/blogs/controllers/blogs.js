20e13916f45e2731b54e952394386cc8f11c6df8
Yousef <yousefsameh24@gmail.com>
2024-04-20 21:07:14 +0200

    solve 13.12

--- partie13/blogs/controllers/blogs.js
+++ partie13/blogs/controllers/blogs.js
@@ -6,7 +6,13 @@ const { Blog } = require('../models')
 const { User } = require('../models')
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name', 'id']
+    }
+  })
   res.json(blogs)
 })
 
