cc77ef498349c31387ffaf46c06e1d8f0021fdaa
Yousef <yousefsameh24@gmail.com>
2024-04-21 19:08:58 +0200

    solve 13.14

--- partie13/blogs/controllers/blogs.js
+++ partie13/blogs/controllers/blogs.js
@@ -7,11 +7,18 @@ const { Blog } = require('../models')
 const { User } = require('../models')
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`
+    where = {
+      [Op.or]: {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`
+        },
+        author: {
+          [Op.iLike]: `%${req.query.search}%`
+        }
+      }
     }
   }
 
