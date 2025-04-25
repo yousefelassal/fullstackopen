e047b888321a1556f201a641d10cbceecb523a43
Yousef Elassal <yousef.elassal@shezlong.com>
2025-04-25 12:33:27 +0300

    solve 4.4

--- part3/backend/utils/list_helper.js
+++ part3/backend/utils/list_helper.js
@@ -1,8 +1,24 @@
+/**
+ * Dummy function that logs blogs and returns 1.
+ * @param {{ _id: string; title: string; author: string; url: string; likes: number; __V: number }} blogs - The blogs to log.
+ * @returns {number} - Always returns 1.
+ */
 const dummy = (blogs) => {
   console.log(blogs);
   return 1;
 };
 
+/**
+ * @param {{ _id: string; title: string; author: string; url: string; likes: number; __V: number }} blogs - The blogs to calculate likes for.
+ * @returns {number} - The total number of likes in the blogs.
+ */
+const totalLikes = (blogs) => {
+  const total = blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  console.log(total);
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
