0dc70d1be903afb5fb7ff92839da0caedc2e573c
Yousef Elassal <yousef.elassal@shezlong.com>
2025-04-25 11:51:36 +0300

    Refactor ESLint configuration and update dependencies
    - Simplified .eslintrc.js formatting and adjusted rule settings.
    - Added eslint-plugin-prettier to package.json and package-lock.json.
    - Updated for_testing.js for consistent formatting.
    - Created .prettierrc for Prettier configuration.

--- part3/backend/.eslintrc.js
+++ part3/backend/.eslintrc.js
@@ -1,46 +1,24 @@
 module.exports = {
-	'env': {
-		'node': true,
-		'commonjs': true,
-		'es2021': true,
-		'jest': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'prettier'
-	],
-	'overrides': [
-	],
-	'parserOptions': {
-		'ecmaVersion': 'latest'
-	},
-	'rules': {
-		'eqeqeq': 'error',
-		'no-trailing-spaces': 'error',
-		'object-curly-spacing': [
-			'error',
-			'always'
-		],
-		'arrow-spacing': [
-			'error',
-			{ 'before': true, 'after': true }
-		],
-		'no-console': 0,
-		'indent': [
-			'error',
-			2
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
-}
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+    indent: 'off',
+    'linebreak-style': 'off',
+    quotes: 'off',
+    semi: 'off',
+  },
+};
